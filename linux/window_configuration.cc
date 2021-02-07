4fe242bb9a4cafa4fd9ad06853970e890bed1ec3
Ashraf Elfeky <43938517+Ashraf-Elfeki@users.noreply.github.com>
2021-02-08 01:49:51 +0300

    Points Counter App - First Commit

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "points_counter";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
